f7e4d5311e421808bdc3d71292a1f93cabfe037c
McLovin <mclovinmohammed1981@gmail.com>
2024-11-07 17:21:43 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,18 +12,10 @@ import {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink>Link</NavigationMenuLink>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {/*web 3 connect wallet button*/}
+          <w3m-button/>
 
-    </main>
+      </main>
   );
 }
